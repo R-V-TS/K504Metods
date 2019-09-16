b76c3fc860871d634bf4df71607bf3aaaaee464a
k504-r <rostislav.tsekhmistro@gmail.com>
2019-09-16 13:47:47 +0300

    choice cpu/gpu in command line [Python mod]

---
+++ src/DCT_Matrices.h
@@ -0,0 +1,23 @@
+//
+// Created by k504-r on 16.09.19.
+//
+
+#pragma once
+#ifndef CUDA__DCT_MATRICES_H
+#define CUDA__DCT_MATRICES_H
+
+namespace ImProcessing{
+    extern float DCT_Creator2[2][2];
+    extern float DCT_Creator2_T[2][2];
+    extern float DCT_Creator4[4][4];
+    extern float DCT_Creator4_T[4][4];
+    extern float DCT_Creator8[8][8];
+    extern float DCT_Creator8_T[8][8];
+    extern float DCT_Creator16[16][16];
+    extern float DCT_Creator16_T[16][16];
+    extern float DCT_Creator32[32][32];
+    extern float DCT_Creator32_T[32][32];
+}
+
+
+#endif //CUDA__DCT_MATRICES_H
